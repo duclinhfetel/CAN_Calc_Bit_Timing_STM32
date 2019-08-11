f89f1acec65faaa5649cd5aa0bc1dc0fb14a8bb5
LinhND <duclinh.fetel@gmail.com>
2019-08-11 11:28:33 +0700

    Update CAN_calc_bit_timing.py

--- CAN_calc_bit_timing.py
+++ CAN_calc_bit_timing.py
@@ -1,20 +1,20 @@
+
 import numpy as np
 
 prescale = np.arange(1,1025)  # 1 -> 1024
 seg1 = np.arange(1,17)        # 1 -> 16
 seg2 = np.arange(1,9)         # 1 -> 8
 
-clock = 36     # Mhz clock input module CAN (APB1 peripheral)
+clock = 42     # Mhz clock input module CAN (APB1 peripheral)
 bitrate = 500  # kbps
-
+sjw = 3
 ratio = (clock*10**6)/(bitrate*1000)
 
-print()
-print ("bit_rate: ", bitrate, "kbps")
-print()
+print("bit_rate: ", bitrate, "kbps")
+
 for i in range(int(ratio)):
-    if (ratio)%prescale[i] == 0:
+    if ratio%prescale[i] == 0:
         for j in seg1:
-            out2 = ((ratio)/prescale[i] - 1) - j
-            if out2 > 0 and out2 <= max(seg2) and (j/(j+out2) >= 0.8):
-                print ("prescale: ", prescale[i], "segment_1: ", j , "segment_2: ", out2, "sample_point: ", j/(j+out2))
+            out2 = int(ratio)/prescale[i] - 1 - j
+            if (out2 > 0) and (out2 <= max(seg2)) and (float(j+1)/(j+out2+1) > 0.65):
+                print("prescale: ", prescale[i], "segment_1: ", j , "segment_2: ", out2, "sample_point: ", (j+1)/(j+out2+1), "SJW: ", sjw)
